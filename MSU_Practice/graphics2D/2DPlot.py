2079893802fd3f80dabbe65aed5ad4f88940175a
Maxim <57135308+maximchaa@users.noreply.github.com>
2020-06-29 05:09:51 +0400

    Add files via upload

---
+++ MSU_Practice/graphics2D/2DPlot.py
@@ -0,0 +1,36 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
+
+
+fig = plt.figure()
+
+ax = fig.gca(projection='3d')
+
+'''
+X = np.arange(-10, 10, 0.1)
+Y = np.arange(-10, 10, 0.1)
+X, Y = np.meshgrid(X, Y)
+Z = X**2 - Y**2
+'''
+
+X=np.loadtxt("C:\\Users\\Administrator.EliteBook-8760w\\Desktop\\2DPlot\\X.txt")
+Y=np.loadtxt("C:\\Users\\Administrator.EliteBook-8760w\\Desktop\\2DPlot\\Y.txt")
+Z=np.loadtxt("C:\\Users\\Administrator.EliteBook-8760w\\Desktop\\2DPlot\\Z.txt")
+# X, Y, Z - матрицы. X из одинаковых строк, Y из одинаковых столбцов, Z из строк, в каждой из которых y фиксируется, а x бегает.
+
+
+
+surf = ax.plot_surface(X, Y, Z, color="g")
+
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
+
+ax.set_xlim(-12, 12)
+ax.set_ylim(-12, 12)
+ax.set_zlim(-110, 110)
+
+fig.suptitle("Чипсы Принглс", fontsize=12)
+
---
+++ MSU_Practice/graphics2D/main.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    ofstream out;
+    double x;
+    double y;
+    int i;
+    int j;
+
+
+    out.open("C:\\Users\\Administrator.EliteBook-8760w\\Desktop\\2DPlot\\X.txt");
+    if (out.is_open())
+    {
+        for(j=0; j<200; j++){
+            for(i=0; i<200; i++)
+            {
+               x=-10+i*0.1;
+               out<<x<<" ";
+            }
+            out<<endl;
+        }
+        out.close();
+    }
+    else
+    {
+        cout<<endl<<"Error opening file"<<endl;
+    }
+
+    out.open("C:\\Users\\Administrator.EliteBook-8760w\\Desktop\\2DPlot\\Y.txt");
+    if (out.is_open())
+    {
+        for(i=0; i<200; i++)
+        {
+              x=-10+i*0.1;
+              for(j=0; j<200; j++)
+              {
+                out<<x<<" ";
+              }
+              out<<endl;
+        }
+        out.close();
+    }
+    else
+    {
+        cout<<endl<<"Error opening file"<<endl;
+    }
+
+    out.open("C:\\Users\\Administrator.EliteBook-8760w\\Desktop\\2DPlot\\Z.txt");
+    if (out.is_open())
+    {
+        for(i=0; i<200; i++)
+        {
+            y=-10+i*0.1;
+            for(j=0; j<200; j++)
+            {
+              x=-10+j*0.1;
+              out<<x*x-y*y<<" ";
+            }
+            out<<endl;
+        }
+        out.close();
+    }
+    else
+    {
+        cout<<endl<<"Error opening file"<<endl;
+    }
+
+    cout<<"done";
+    return 0;
+}
